0bbcda8ac37a32996cdaad93a23b14f7751d5b38
Jorge Boscan <jboscan@classy.org>
2021-03-13 18:01:40 -0300

    Fix para valor del dolar durante fin de semana

--- src/core/api.js
+++ src/core/api.js
@@ -2,7 +2,11 @@
 export const DEFAULT_USD_VALUE = 700;
 
 export async function fetchUsdValue() {
-  const date = new Date();
+  var date = new Date();
+  if (date.getDay() > 5) {
+    date = new Date(date.setDate(date.getDate() - (date.getDay() - 5)))
+  }
+
   const month = date.getMonth() + 1;
   const today = `${date.getDate()}-${month < 10 ? `0${month}` : month}-${date.getFullYear()}`;
   try {
