727e81f12a502bf8a7d4f70d7823e072dcbca6aa
Nicolás Gómez <ngomez@hey.com>
2023-01-03 13:30:18 -0300

    fix: configure current year at 2022 max
    fix #17

--- src/core/calculator.js
+++ src/core/calculator.js
@@ -1,6 +1,6 @@
 import { calcular, obtenerConfiguracion, configurarDeclaracion } from "tax-cl";
 
-configurarDeclaracion(new Date().getFullYear() + 1);
+configurarDeclaracion(Math.min(new Date().getFullYear() + 1), 2022);
 
 const { RETENCION, COBERTURA_PARCIAL } = obtenerConfiguracion();
 
